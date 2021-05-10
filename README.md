9007ebdc5169a0d2d160c1290d2fb28621f581c7
Reuben <rleibgenkin@gmail.com>
2021-05-10 01:52:15 -0400

    final touches

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# zhora
+# Random Password Generator
+
+Using random number generators and if else statements I completed the assignment to the extent of the brief. <br />
+The if else statements were used to filter out the users requirements for the password.
+
+[Link to deployed website](https://reubengenkin.github.io/zhora/)
+
+[Link to git hub repository](https://github.com/ReubenGenkin/zhora)
+
+![screenshot](./zhora/screencapture-file-C-Users-rleib-Desktop-Rutgers-Coding-Homework-challenge3-zhora-index-html-2021-05-10-01_45_04 (1).png)
+
--- js.js
+++ js.js
@@ -9,39 +9,42 @@ var numb = ["1234567890"]
 var passwordCheck = ""
 
 function check() {
-    var lengthCheck= prompt("How many characters do you want?");
+    var lengthCheck = prompt("How many characters do you want?");
 
-    if ( lengthCheck <= 128 && lengthCheck >= 8 ) {
-        alert ("Please choose password parameters from the next 4 prompts");
+    if (lengthCheck <= 128 && lengthCheck >= 8) {
+        alert("Please choose password parameters from the next 4 prompts");
         passLength = parseInt(lengthCheck);
 
         upperCheck = confirm("Do you want uppercase letters?");
-            if ( upperCheck) {
-                passwordCheck = passwordCheck + upper
-            } else {
-            }
+        if (upperCheck) {
+            passwordCheck = passwordCheck + upper
+        } else {
+        }
 
         lowerCheck = confirm("Do you want lowercase letters?");
-            if ( lowerCheck) {
-                passwordCheck = passwordCheck + lower
-            } else {}
+        if (lowerCheck) {
+            passwordCheck = passwordCheck + lower
+        } else {
+        }
 
         numbCheck = confirm("Do you want numbers?");
-            if ( numbCheck) {
-                passwordCheck = passwordCheck + numb
-            } else {}
+        if (numbCheck) {
+            passwordCheck = passwordCheck + numb
+        } else {
+        }
 
         charCheck = confirm("Do you want special characters?");
-            if (charCheck) {
-                passwordCheck = passwordCheck + char
-    
-            } else {}
+        if (charCheck) {
+            passwordCheck = passwordCheck + char
 
-    } else if ( lengthCheck < 8 || lengthCheck > 128) {
-        alert ("Password must be between 8 and 128 characters");
+        } else {
+        }
+
+    } else if (lengthCheck < 8 || lengthCheck > 128) {
+        alert("Password must be between 8 and 128 characters");
         check();
     }
-    
+
 }
 
 /*check();
@@ -53,13 +56,13 @@ function randomGen() {
     var passwordText = document.querySelector("#password");
 
     var passEnd = "";
-    for (var i= 0; i < passLength; i++) {
-      passEnd += passwordCheck[Math.floor(Math.random() * passwordCheck.length)];
+    for (var i = 0; i < passLength; i++) {
+        passEnd += passwordCheck[Math.floor(Math.random() * passwordCheck.length)];
     }
-    
-    
+
+
     passwordText.value = passEnd;
-  
+
 }
 
 
